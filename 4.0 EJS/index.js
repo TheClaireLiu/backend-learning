368550cd7e06b4011dcc54172f3da6b79f4d8ed2
TheClaireLiu <cliu202211@gmail.com>
2024-07-08 18:22:02 -0400

    [Add]4.0 ESJ folder about Server-Side Render

---
+++ 4.0 EJS/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+// app.set("view engine", "ejs");
+
+app.get("/", (req,res) => {
+    const day = new Date();
+    const today = day.getDay();
+    let type = "a weekday";
+    let adv = "it's time to work hard";
+
+    if(today === 0 || today === 6){
+        type = "a weekend";
+        adv = "it's time to have some fun";
+    }
+    res.render("index.ejs", {
+        dataType: type,
+        advice: adv,
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
