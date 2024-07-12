7f3151279864808d8213cd990fd1a9e9aa3a5a69
TheClaireLiu <cliu202211@gmail.com>
2024-07-11 23:45:17 -0400

    [Update] fix the index.js

--- 4.4 Band Generator Project/index.js
+++ 4.4 Band Generator Project/index.js
@@ -2,13 +2,14 @@ import express from "express";
 import bodyParser from "body-parser";
 
 const app = express();
-const port = 3001;
+const port = 3000;
 
 //Step 3 - Make the styling show up.
 //Hint 1: CSS files are static files!
 //Hint 2: The header and footer are partials.
 //Hint 3: Add the CSS link in header.ejs
 app.use(express.static("public"));
+
 //Step 4 - Add a dynamic year to the footer.
 //Hint: Google to find out how to get the current year using JS.
 
@@ -20,8 +21,8 @@ app.get("/", (req, res) => {
 
 app.post("/submit", (req, res) => {
   console.log("hello")
-  const randomAdj = adj[Math.floor(Math.random * adj.length)];
-  const randomNoun = noun[Math.floor(Math.random * noun.length)];
+  const randomAdj = adj[Math.floor(Math.random() * adj.length)];
+  const randomNoun = noun[Math.floor(Math.random() * noun.length)];
   
   res.render("index.ejs", {
     randAdjective: randomAdj,
